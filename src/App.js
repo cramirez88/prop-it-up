f56a4fc3893d9516bd2f8cee4d2f269361627012
Christian Ramirez <christian.r0602@gmail.com>
2022-05-06 11:54:30 -0500

    Passing props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PersonalCard from './components/PersonCard'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PersonalCard 
+      lname = {'Doe'}
+      fname = {'Jane'}
+      age = {45}
+      hairColor = {'Black'}
+      />
+      <PersonalCard 
+      lname = {'Smith'}
+      fname = {'John'}
+      age = {88}
+      hairColor = {'Brown'}
+      />
+      <PersonalCard 
+      lname = {'Fillmore'}
+      fname = {'Millard'}
+      age = {50}
+      hairColor = {'Brown'}
+      />
+      <PersonalCard 
+      lname = {'Smith'}
+      fname = {'Maria'}
+      age = {62}
+      hairColor = {'Brown'}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
